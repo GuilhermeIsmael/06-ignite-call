e1a85bfce47152afbe4505df7a0f4fde1ca763c8
Guilherme Ismael <guiismael@gmail.com>
2023-04-02 20:23:43 -0300

    Initial commit

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,32 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { SessionProvider } from 'next-auth/react'
+import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+
+import '@/lib/dayjs'
+import { queryClient } from '@/lib/react-query'
+import { globalStyles } from '../styles/global'
+
+globalStyles()
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://ignitecall.guilhermeismael.dev',
+            siteName: 'Ignite Call',
+          }}
+        />
+
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
+  )
+}
---
+++ src/pages/api/users/time-intervals.api.ts
@@ -0,0 +1,52 @@
+import { getServerSession } from 'next-auth'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+import { prisma } from '@/lib/prisma'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          start_time_in_minutes: interval.startTimeInMinutes,
+          end_time_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
+}
